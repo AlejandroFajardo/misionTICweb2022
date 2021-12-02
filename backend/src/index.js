208f917b40aeaf24f8f381de385a1cb8266579be
Viviana Murillo <=>
2021-12-02 14:04:36 -0500

    Creación de archivos back

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+import validateAuthentication from './middlewares/authentication.middleware.js';
+import connect from './database.js';
+import typeDefs from './schema/index.js';
+import resolvers from './resolvers/index.js';
+
+dotenv.config();
+connect();
+
+const startApolloServer = async (typeDefs, resolvers) => {
+
+  const PORT = process.env.PORT;
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: async ({ req }) => await validateAuthentication(req),
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startApolloServer(typeDefs, resolvers);
---
+++ backend/src/resolvers/misc.resolver.js
@@ -0,0 +1,26 @@
+// import axios from 'axios';
+
+const roles = async () => {
+    // const response = await axios.get('http://localhost:8080/roles');
+  
+    return [
+      {
+        code: 'ADMIN',
+        value: 'Administrador',
+      },
+      {
+        code: 'LEADER',
+        value: 'Líder',
+      },
+      {
+        code: 'STUDENT',
+        value: 'Estudiante',
+      }
+    ];
+  };
+  
+  export default {
+    miscQueries: {
+      roles,
+    }
+  }
